94a99dbddb4cd9a6ed25c013635a6262e4af792a
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2023-11-03 11:28:31 +0700

    implement two pivot block quicksort

--- two_pivot_block_quicksort.py
+++ two_pivot_block_quicksort.py
@@ -1,30 +1,45 @@
 BLOCK_SIZE = 16
-NUM_OF_BLOCKS = _
 
 
-def sort(arr):
-    if arr[0] > arr[-1]:
-        arr[0], arr[-1] = arr[-1], arr[0]
-    p1, p2 = arr[0], arr[-1]
-    block = [0] * NUM_OF_BLOCKS 
-    i, j, k = 2, 2, 2 
+def two_pivot_quicksort(arr, l, r):
+    if (l < r):
+        i, j = block_lomuto_two_pivot(arr,l,r)
+        two_pivot_quicksort(arr,l,i-1)
+        two_pivot_quicksort(arr,i+1,j-1)
+        two_pivot_quicksort(arr,j+1,r)
+
+
+def block_lomuto_two_pivot(arr, l, r):
+    if arr[l] > arr[r]: 
+        arr[l], arr[r] = arr[r], arr[l]
+    p1, p2 = arr[l], arr[r]
+    block = [0] * BLOCK_SIZE
+    i, j, k = l+1, l+1, l+1
     lt_p1, le_p2 = 0, 0
-    while k < len(arr):
-        t = min(NUM_OF_BLOCKS, len(arr)-k)
-        for c in range(0, le_p2):
-            arr[j+c], arr[k+block[c]] = arr[k+block[c]], arr[j+k]
+    while k < r:
+        t = min(BLOCK_SIZE, r-k)
+        for c in range(t):
+            block[le_p2] = c
+            le_p2 += (arr[k+c] <= p2)
+        for c in range(le_p2):
+            arr[j+c], arr[k+block[c]] = arr[k+block[c]], arr[j+c]
         k += t
-        for c in range(0, le_p2):
+        for c in range(le_p2):
             block[lt_p1] = c
-            lt_p1 += (p > arr[j+c])
-        for c in range(0, lt_p1):
+            lt_p1 += (arr[j+c] < p1)
+        for c in range(lt_p1):
             arr[i], arr[j+block[c]] = arr[j+block[c]], arr[i]
             i += 1
         j += le_p2
         lt_p1, le_p2 = 0, 0
-    arr[i-1], arr[1] = arr[1], arr[i-1]
-    arr[j], arr[-1] = arr[-1], arr[j]
+    arr[i-1], arr[l] = arr[l], arr[i-1]
+    arr[j], arr[r] = arr[r], arr[j]
     return (i-1, j)
+
+
+def sort(arr):
+    two_pivot_quicksort(arr, 0, len(arr)-1)
+    return arr
 
 
 def main():
